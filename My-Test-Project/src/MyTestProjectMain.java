cf12449b48a915e06559aeef0c73b3ef81ebd7a1
daltonmartin <daltonmartin@192.168.0.22>
2020-04-01 12:01:28 -0400

    This is another commit message.

--- My-Test-Project/src/MyTestProjectMain.java
+++ My-Test-Project/src/MyTestProjectMain.java
@@ -4,6 +4,7 @@ public class MyTestProjectMain {
 	public static void main(String[] args) {
 		
 		System.out.println("Test project to learn Git stuff...");
+		System.out.println("Another change.");
 		System.exit(0);
 
 	}
